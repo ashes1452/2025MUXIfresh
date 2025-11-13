8cc4fec6b7d0f9ce4b52eb8789be7086917c14ef
ashes1452 <1587094537@qq.com>
2025-11-13 18:36:07 +0800

    modified:   "\344\275\234\344\270\232/task3.js"

--- 作业/task3.js
+++ 作业/task3.js
@@ -77,3 +77,58 @@ console.log(reverseArrayInPlace([1, 2, 3, 4, 5]));
 console.log(reverseArrayInPlace([1, 2, 3, 4]));
 
 //列表
+function arrayTolist(arr) {
+    let list = null;
+    for (let i = arr.length - 1; i >= 0; i--) {
+        list = { value: arr[i], rest: list };
+    }
+    return list;
+}
+function listToArray(list) {
+    let arr = [];
+    for (let node = list; node !== null && node !== undefined; node = node.rest) {
+        arr.push(node.value);
+    }
+    return arr;
+}
+function prepend(value, list) {
+    let newlist = { value: value, rest: list };
+    return newlist;
+}
+function nth(list, num) {
+    let arr = [];
+    for (let node = list; node !== null && node !== undefined; node = node.rest) {
+        arr.push(node.value);
+    }
+    return arr[num];
+}
+console.log(arrayTolist([10, 20, 30]));
+console.log(listToArray(arrayTolist([10, 20, 30])));
+console.log(prepend(10, prepend(20, null)));
+console.log(nth(arrayTolist([10, 20, 30]), 1));
+
+//深度比较
+function deepEqual(obj1, obj2) {
+    if (obj1 === obj2) {
+        return true;
+    }
+    else if (obj1 == null || typeof obj1 != 'object' || obj2 == null || typeof obj2 != 'object') {
+        return false;
+    }
+    let keys1 = Object.keys(obj1);
+    let keys2 = Object.keys(obj2);
+    if (keys1.length != keys2.length) {
+        return false;
+    }
+    for (let key of keys1) {
+        if (!keys2.includes(key) || !deepEqual(obj1[key], obj2[key])) {
+            return false
+        }
+
+    }
+    return true;
+}
+let obj = { here: { is: "an" }, object: 2 };
+console.log(deepEqual(obj, obj));
+console.log(deepEqual(obj, { here: 1, object: 2 }));
+console.log(deepEqual(obj, { here: { is: "an" }, object: 2 }));
